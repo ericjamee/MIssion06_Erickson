db5ff6590a1a40606b42cf436b16df72f40e6c78
James Erickson <je7erickson@gmail.com>
2025-02-15 00:12:30 -0700

    Finished website

---
+++ MIssion06_Erickson/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MIssion06_Erickson.Models;
+using System.Linq;
+
+namespace MIssion06_Erickson.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = _context.Movies.ToList();
+            return View(movies);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+    }
+}
---
+++ MIssion06_Erickson/Views/Movies/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MIssion06_Erickson.Views.Movies
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
